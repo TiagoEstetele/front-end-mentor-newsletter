4d5b30b973a33f6af79c29cdf3e987f7f843051f
Tiago Estetele <tiagoestetele@hotmail.com>
2023-06-06 00:38:16 -0300

    Ajustes principais

---
+++ assets/js/show.js
@@ -0,0 +1,8 @@
+let button = $j('.h-call__container button');
+
+button.addEventListener('click', () => {
+    $j('.h-call').classList.add('hidde');
+    setTimeout(() => $j('.h-call').style.display = "none", 350);
+    setTimeout(() => $j('.h-newsletter').style.display = "flex", 355);
+    setTimeout(() => $j('.h-newsletter').classList.add('show'), 360);
+});
